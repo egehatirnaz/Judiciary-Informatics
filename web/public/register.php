9c4b94e6763a8175872095d21c9748e90aa20abe
Ege Hatırnaz <egehatirnaz@hotmail.com>
2019-05-10 12:12:59 +0300

    Frontend of Home & Login & Register is completed with DOM validation.

---
+++ web/public/register.php
@@ -0,0 +1,206 @@
+<?php
+include 'db_connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>National Judiciary Informatics System - Register</title>
+    <meta name="description" content="User registiration page for National Judiciary Informatics System.">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        .login_description {
+            margin: 0;
+            margin-bottom: 5px;
+        }
+
+        .login_input {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
+<body id="body">
+    <!--==========================
+    Header
+  ============================-->
+    <header id="header">
+        <div class="container">
+
+            <div id="logo" class="pull-left">
+                <h1><a href="/" class="scrollto">NJIS - <span>National Judiciary Informatics System</span></a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+            </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/login.php">Login</a></li>
+                    <li class="menu-active"><a href="/register.php">Register</a></li>
+                    <!--
+                    <li class="menu-has-children"><a href="">Drop Down</a>
+                        <ul>
+                            <li><a href="#">Drop Down 1</a></li>
+                            <li><a href="#">Drop Down 3</a></li>
+                            <li><a href="#">Drop Down 4</a></li>
+                            <li><a href="#">Drop Down 5</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Contact</a></li>
+                    -->
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </header><!-- #header -->
+
+    <main id="main">
+        <section id="login">
+            <div class="container" style="margin-top:40px;">
+                <div class="section-header">
+                    <h2>Login</h2>
+                    <p>Please enter your personal information to register to system. All fields are required.</p>
+                </div>
+                <div class="form">
+                    <form action="" method="POST" role="form" class="contactForm">
+                        <!-- Personal Information -->
+                        <h4>Personal Information</h4>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">National ID:</p>
+                                    <input class="login_input" type="text" name="user_id" data-rule="minlen:11" data-msg="Your ID must be 11 digits long.">
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Surname:</p>
+                                    <input class="login_input" type="text" name="user_surname" data-rule="required" data-msg="This field is required.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Name:</p>
+                                    <input class="login_input" type="text" name="user_name" data-rule="required" data-msg="This field is required.">
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">E-mail:</p>
+                                    <input class="login_input" type="text" name="user_email" data-rule="email" data-msg="Please enter a valid email.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Phone Number:</p>
+                                    <input class="login_input" type="text" name="user_phone" data-rule="required" data-msg="This field is required.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Password -->
+                        <h4>Password</h4>
+                        <div class="row" style="margin-top:20px;">
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Password (min. 8 characters):</p>
+                                    <input class="login_input" type="password" name="user_pass" data-rule="minlen:8" data-msg="Please enter at least 8 characters.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Confirm Password:</p>
+                                    <input class="login_input" type="password" name="user_pass_confirm" data-rule="minlen:8" data-msg="Please enter at least 8 characters.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Address Information -->
+                        <h4>Address Information</h4>
+                        <div class="row" style="margin-top:20px;">
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Address:</p>
+                                    <textarea class="login_input" type="text" name="user_address" data-rule="required" data-msg="This field is required."></textarea>
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">City:</p>
+                                    <input class="login_input" type="text" name="user_city" data-rule="required" data-msg="This field is required.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="box wow fadeInLeft form-group">
+                                    <p class="login_description">Zip Code:</p>
+                                    <input class="login_input" type="text" name="user_zipcode" data-rule="required" data-msg="This field is required.">
+                                    <div class="validation"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <div class="box wow fadeInLeft">
+                                    <input class="signUpButton" type="submit" value="Register">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section><!-- #login -->
+    </main>
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script>
+
+    <!-- Contact Form JavaScript File -->
+    <script src="contactform/contactform.js"></script>
+
+    <!-- Template Main Javascript File -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
